e344d10f2e34de54ccc547cac17d06aa460d19bb
gr4viton <lordmutty@gmail.com>
2016-11-21 20:01:28 +0000

    stream it without sudo takes the lib path from .bashrc

--- script/stream_it.sh
+++ script/stream_it.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo mjpg_streamer -i "/usr/local/lib/input_file.so -f ../script/ -n out.mjpg" -o "/usr/local/lib/output_http.so -w /usr/local/www -p 8080"
+#sudo mjpg_streamer -i "/usr/local/lib/input_file.so -f ../script/ -n out.mjpg" -o "/usr/local/lib/output_http.so -w /usr/local/www -p 8080"
+mjpg_streamer -i "input_file.so -f ../script/ -n out.mjpg" -o "output_http.so -w /usr/local/www -p 8080"
 
 
